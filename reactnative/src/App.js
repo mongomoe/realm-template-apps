70267f429888c5d1cd99de5cf1546dd4e945b821
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2021-03-16 01:11:07 -0400

    added navigation screens

--- reactnative/src/App.js
+++ reactnative/src/App.js
@@ -8,14 +8,48 @@
 
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 import Realm from 'realm';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+function WelcomeView({navigation}) {
+  return (
+    <SafeAreaProvider>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Welcome Screen</Text>
+        <Button
+          title="Go to Tasks Screen"
+          onPress={() => navigation.navigate('Tasks')}
+        />
+      </View>
+    </SafeAreaProvider>
+  );
+}
+function TaskView({navigation}) {
+  return (
+    <SafeAreaProvider>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Tasks Screen</Text>
+        <Button
+          title="Go to Welcome Screen"
+          onPress={() => navigation.navigate('Welcome')}
+        />
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <Text style={styles.highlight}>Hello world</Text>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={WelcomeView} />
+        <Stack.Screen name="Tasks" component={TaskView} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
