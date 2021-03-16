a0958ac7a0c48da83b41cc25523efc40bbadbfc3
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2021-03-16 02:04:44 -0400

    added login inputs

--- reactnative/src/views/WelcomeView.js
+++ reactnative/src/views/WelcomeView.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {StyleSheet, Text, View, Button} from 'react-native';
+import {Input} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+Icon.loadFont();
 
 export function WelcomeView({navigation, route}) {
+  const [passwordHidden, setPasswordHidden] = useState(true); // true by default
+
   return (
     <SafeAreaProvider>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Welcome Screen</Text>
+        <Text style={styles.title}>My Sync App</Text>
+        <Input placeholder="email" />
+        <Input
+          placeholder="password"
+          secureTextEntry={passwordHidden}
+          rightIcon={
+            <Icon
+              name={passwordHidden ? 'eye-slash' : 'eye'}
+              size={12}
+              color="black"
+              onPress={() => setPasswordHidden(!passwordHidden)}
+            />
+          }
+        />
+
         <Button
           title="Go to Tasks Screen"
           onPress={() => navigation.navigate('Tasks')}
@@ -17,6 +36,9 @@ export function WelcomeView({navigation, route}) {
 }
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+  },
   //   sectionContainer: {
   //     marginTop: 32,
   //     paddingHorizontal: 24,
